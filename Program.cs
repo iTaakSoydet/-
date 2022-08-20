afc00b609ee612cc5df1f42538206dd970927fef
AndreyLitvinov <a.litvinov92@bk.ru>
2022-08-20 23:15:51 +0300

    wrote a program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите максимальное кол-во символов в элементе финального массива: ");
+int maxSimbols = Convert.ToInt32(ReadLine());
+WriteLine("Введите кол-во элементов в массиве: ");
+int initialArrayLength = Convert.ToInt32(ReadLine());
+
+string[] initialArray = GetArray(initialArrayLength);
+string[] finalArray = GetFinalArray(maxSimbols, initialArray);
+PrintArray(initialArray);
+PrintArray(finalArray);
+
+string[] GetArray(int size)
+{
+    string[] result = new string[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        WriteLine($"Введите {i + 1} элемент массива: ");
+        result[i] = ReadLine();
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Write("[");
+    for (int j = 0; j < array.Length; j++)
+    {
+        Write($"''{array[j]}''");
+        if (j < array.Length - 1)
+        {
+            Write(", ");
+        }
+    }
+    WriteLine("]");
+}
+
+string[] GetFinalArray(int max, string[] array)
+{
+    string[] result = new string[0];
+    int i = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length <= max)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[i] = array[j];
+            i = i + 1;
+        }
+    }
+    return result;
+}
+
+
+
